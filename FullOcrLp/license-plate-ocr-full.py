76d639709f8029c74d6ba87fa3b623aadb2c14c8
BattsEA <b-eugen@yandex.ru>
2018-12-24 15:45:06 +0300

    LP ocr

--- FullOcrLp/license-plate-ocr-full.py
+++ FullOcrLp/license-plate-ocr-full.py
@@ -2,7 +2,7 @@ import os
 import recognize
 import cv2
 
-IMG_PATH = '/mnt/misk/misk/lplate/images'
+IMG_PATH = 'E:/temp/images'
 IMG_FOR_OCR = '/mnt/misk/misk/lplate/chars'
 
 
@@ -11,6 +11,8 @@ def main():
     for file in os.listdir(IMG_PATH):
         fp = os.path.join(IMG_PATH, file)
         img = cv2.imread(fp, cv2.IMREAD_GRAYSCALE)
+        x1, x2, y1, y2 = (125, 1070, 380, 830)
+        img = img[y1:y2, x1:x2]
         fn = os.path.splitext(file)[0].split('_')
         rc.recognize(img, fn)
 
--- FullOcrLp/recognize.py
+++ FullOcrLp/recognize.py
@@ -19,16 +19,16 @@ PREDICT_DETECT_LEVEL = 0.7
 
 
 class RecognizeLp(object):
-    def __init__(self)   :
+    def __init__(self):
         self.cntf = 0
         self.folder_nn = 'nn/'
         self.nn_detect_lp = 'model-detect-lp'
         self.nn_ocr_lp = 'model-ocr-lp'
-        self.letters = sorted(list(LP_LETTERS))
-        self.max_len = LP_LETTERS
-        self.pdl = PREDICT_DETECT_LEVEL
         self.dlp = self.__get_model_detect_lp()
         self.ocrlp = self.__get_model_ocr_lp()
+        self.pdl = 0.7
+        self.letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+                        'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y', ' ']
 
 
     def __plot_images(self, images, grey):
@@ -77,170 +77,88 @@ class RecognizeLp(object):
             md_arr.append(md)
             out = img[min_y:max_y + 1, min_x:max_x + 1]
             out = self.__image_rotate(out, i)
-            out = cv2.resize(out, (128, 64))
+            #out = cv2.resize(out, (128, 64))
             # out = np.expand_dims(out.T, -1)/255
             img_gepotise.append(out)
         mdmax = np.max(md_arr)
         maxs = np.where((md_arr >= np.uint32(mdmax)) & (md_arr <= mdmax))
         return np.array(img_gepotise)[maxs]
 
-    def __char_crop(self, img, mean_size=3, median_k_a=1.1, pix_shift_back=2, pix_shoft_forw=2,
-                    char_size_min=6):
-        mean_imgs = []
-        imgs = []
-        for i in range(0, img.shape[1]):
-            mean_imgs.append(np.mean(img[:, i:i + mean_size]))
-            i += mean_size
-        med_all_img = np.median(mean_imgs) * median_k_a
-        index = np.where(mean_imgs >= med_all_img)
-        mean_imgs = np.array(mean_imgs)
-        mean_imgs[index] = np.max(mean_imgs)
-        index = np.where(mean_imgs >= np.max(mean_imgs))
-        if len(index) > 0:
-            index = index[0]
-        if index[0] > (2 * mean_size):
-            index = np.insert(index, 0, mean_size)
-        for i in range(1, len(index)):
-            if (index[i] - index[i - 1]) < char_size_min:
-                continue
-            li = index[i - 1] - pix_shift_back if index[i - 1] - pix_shift_back >= 0 else index[i - 1]
-            ri = index[i] + pix_shoft_forw if index[i] + pix_shoft_forw <= img.shape[1] else img.shape[1]
-            imgs.append(np.copy(img[0:img.shape[0], li:ri]))
-        return imgs
-
-    def __lp_crop(self, img, mean_size=2, max_crop=5):
-        mean_imgs = []
-        for i in range(0, img.shape[0]):
-            mean_imgs.append(np.mean(img[i:i + mean_size, :]))
-            i += mean_size
-        med_all_img = np.mean(mean_imgs) * 0.8
-        index = np.where(mean_imgs < med_all_img)
-        hh = img.shape[0] * 0.5
-        index = np.squeeze(index, -1)
-        sl = index[index >= hh]
-        if len(sl) > 0:
-            l_top = np.min(sl)
-            l_top = l_top if img.shape[0] - l_top <= max_crop else img.shape[0] - max_crop
-            img[l_top: img.shape[0], :] = 255
-        sl = index[index <= hh]
-        if len(sl) > 0:
-            l_bot = np.max(sl)
-            l_bot = l_bot if l_bot <= max_crop else max_crop
-            img[0:l_bot, :] = 255
-        return img
-
-    def __img_split(self, images, img_max_lenght=20):
-        img_wb = []
-        for i in range(0, len(images)):
-            if images[i].shape[1] > img_max_lenght:
-                img_wb.append(i)
-        off_corr = 0
-        for i in img_wb:
-            imgs_split = self.__char_crop(images[i + off_corr], pix_shift_back=3, pix_shoft_forw=3)
-            if len(imgs_split) > 0:
-                images[i + off_corr] = imgs_split[0]
-                inx = i + 1 + off_corr
-                for y in range(1, len(imgs_split)):
-                    images.insert(inx, imgs_split[y])
-                    inx += 1
-            off_corr += len(imgs_split) - 1
+    def __get_split_mask(self, image, char_size_min=8, char_size=10):
+        img = cv2.resize(image, (128, 64))
+        mask_index_split = []
+        kernel = np.ones((2, 2), np.uint8)
+        img = cv2.erode(img, kernel, iterations=2)
+        ret, img = cv2.threshold(img, 50, 255, 1)
+        mean_imgs = np.mean(img, axis=0)
+        imgplot = plt.imshow(img, cmap='gray')
+        plt.plot(mean_imgs)
+        plt.show()
+        i = 0
+        fi = int(char_size / 2)
+        index = np.where(mean_imgs[i:i + fi] == np.min(mean_imgs[i:i + fi]))
+        i = np.min(index)
+        mask_index_split.append(i)
+        while (i + char_size) < len(mean_imgs):
+            index = np.where(mean_imgs[i:i + char_size] == np.min(mean_imgs[i:i + char_size]))
+            n = np.min(index) + i
+            if len(mask_index_split) and (n - mask_index_split[len(mask_index_split) - 1]) <= char_size_min:
+                i += char_size
+            else:
+                mask_index_split.append(n)
+                i = n + int(char_size / 2)
+        images = []
+        for i in range(1, len(mask_index_split)):
+            images.append(img[:, mask_index_split[i - 1]: mask_index_split[i]])
         return images
 
-    #         cv2.imshow("Contour", cv2.resize(img, (img.shape[1] * 3, img.shape[0] * 3)))
-    #         cv2.waitKey(2000)
-    def __img_crop(self, img, mean_size=2, level_blank=2, invert=False):
-        mean_imgs = []
-        for i in range(0, img.shape[0]):
-            mean_imgs.append(np.mean(img[i:i + mean_size, :]))
-            i += mean_size
-        index = np.where(mean_imgs >= np.int32(level_blank)) if invert else np.where(mean_imgs <= np.int32(level_blank))
-        hh = img.shape[0] * 0.5
+    def __img_crop_next(self, img, level_blank=5, axis=0, lt=True):
+        mean_imgs = np.mean(img, axis=axis)
+        index = np.where(mean_imgs <= level_blank) if lt else np.where(mean_imgs >= level_blank)
+        if axis == 0:
+            hw = img.shape[1] * 0.5
+        else:
+            hw = img.shape[0] * 0.5
+        shape_img = img.shape[1] if axis == 0 else img.shape[0]
         index = np.squeeze(index, -1)
-        sl = index[index >= hh]
-        l_top = np.min(sl) - 1 if len(sl) > 0 else img.shape[0] - 1
-        sl = index[index <= hh]
-        l_bot = np.max(sl) + 1 if len(sl) > 0 else 1
-        w = img.shape[1]
-        h = l_top - l_bot
+        sl = index[index > hw]
+        l_top = np.min(sl) - 1 if len(sl) > 0 else shape_img
+        l_top = l_top if l_top >= 0 else shape_img
+        sl = index[index < hw]
+        l_bot = np.max(sl) + 1 if len(sl) > 0 else 0
+        l_bot = l_bot if l_bot >= 0 else 0
+        w = img.shape[1] if axis > 0 else l_top - l_bot
+        h = img.shape[0] if axis == 0 else l_top - l_bot
+        print(w, h)
+        if h < 0:
+            h = img.shape[0]
+        if w < 0:
+            w = img.shape[1]
         imc = np.zeros((h, w))
-        imc[:, :] = img[l_bot:l_top, :]
-        return np.uint8(imc)
-
-    def __img_crop_next(self, img, level_blank=5, axis=0):
-        try:
-            mean_imgs = np.mean(img, axis=axis)
-            index = np.where(mean_imgs <= level_blank)
-            if axis == 0:
-                hw = int(np.mean(np.where(mean_imgs[20:44] == np.max(mean_imgs[20:44])))) + 20
-            else:
-                hw = img.shape[0] * 0.5
-            shape_img = img.shape[1] if axis > 0 else img.shape[0]
-            index = np.squeeze(index, -1)
-            sl = index[index > hw]
-            l_top = np.min(sl) - 1 if len(sl) > 0 else shape_img
-            l_top = l_top if l_top >= 0 else shape_img
-            sl = index[index < hw]
-            l_bot = np.max(sl) + 1 if len(sl) > 0 else 0
-            l_bot = l_bot if l_bot >= 0 else 0
-            w = img.shape[1] if axis > 0 else l_top - l_bot
-            h = img.shape[0] if axis == 0 else l_top - l_bot
-            if h < 0:
-                h = img.shape[0]
-            if w < 0:
-                w = img.shape[1]
-            imc = np.zeros((h, w))
-            imc[:, :] = img[l_bot:l_top, :] if axis > 0 else img[:, l_bot:l_top]
-            return imc
-        except:
-            return None
+        imc[:, :] = img[l_bot:l_top, :] if axis > 0 else img[:, l_bot:l_top]
+        return imc
 
     def __image_normalisation(self, image):
         try:
-            koeff = 60 / image.shape[0]
-            (h, w) = (int(image.shape[0]*koeff), int(image.shape[1]*koeff))
-            image = 255-cv2.resize(image, (h, w))
+            image = cv2.resize(image, (24, 38))
             imr = np.zeros((64, 64))
-            yo = int(0.5*64 - image.shape[0]*0.5)
-            xo = int(0.5*64 - image.shape[1]*0.5)
+            yo = int(0.5 * 64 - image.shape[0] * 0.5)
+            xo = int(0.5 * 64 - image.shape[1] * 0.5)
             imr[yo:image.shape[0] + yo, xo:image.shape[1] + xo] = image[0:image.shape[0], 0:image.shape[1]]
             return imr
         except:
             return None
 
-    def __image_conversion(self, image, lp_number):
+    def __image_conversion(self, imglp, lp_number):
         print(lp_number)
-        image = cv2.GaussianBlur(image, (5, 5), 0)
-        image = cv2.adaptiveThreshold(image, 200, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 3)
-        kernel = np.ones((1, 1), np.uint8)
-        image = cv2.erode(image, kernel, iterations=1)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 1))
-        ret, image = cv2.threshold(image, 100, 255, 0)
-        image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-        image = self.__lp_crop(image)
-        images = self.__char_crop(image)
-        images = self.__img_split(images)
-        self.__plot_images(images, True)
+        images = self.__get_split_mask(imglp)
+        kernel = np.ones((3, 1), np.uint8)
         for i in range(0, len(images)):
-            img = self.__img_crop(images[i])
-            if img is None:
-                continue
-            img = self.__img_crop(img, level_blank=251, invert=True)
-            if img is None:
-                continue
-            img = self.__image_normalisation(img)
-            if img is None:
-                continue
-            img = self.__img_crop_next(img)
-            if img is None:
-                continue
-            img = self.__img_crop_next(img, axis=1)
-            if img is None:
-                continue
-            imr = np.zeros((64, 64))
-            yo = int(0.5 * 64 - img.shape[0] * 0.5)
-            xo = int(0.5 * 64 - img.shape[1] * 0.5)
-            imr[yo:img.shape[0] + yo, xo:img.shape[1] + xo] = img[0:img.shape[0], 0:img.shape[1]]
-            images[i] = imr
+            images[i] = self.__img_crop_next(images[i], level_blank=240, axis=1, lt=False)
+            images[i] = self.__img_crop_next(images[i], axis=0)
+            images[i] = cv2.erode(images[i], kernel, iterations=2)
+            images[i] = self.__img_crop_next(images[i], axis=1)
+            images[i] = self.__image_normalisation(images[i])
         return images
 
     def __image_rotate(self, img, angle):
@@ -252,7 +170,7 @@ class RecognizeLp(object):
     def __image_ocr(self, images):
         images = np.array(images)/255
         images = np.reshape(images, images.shape + (1, ))
-        predict = self.ocrlp.predict(images)
+        predict = self.ocrlp.predict_classes(images)
         return predict
 
     def recognize(self, image, file):
@@ -260,8 +178,11 @@ class RecognizeLp(object):
         if img is not None:
             for im in img:
                 images = self.__image_conversion(im, file)
-                lp = self.__image_ocr(images)
-            # self.__ocr_license_plate(img)
+                lps = self.__image_ocr(images)
+                number = ''
+                for ch in lps:
+                    number += self.letters[ch]
+                print(number)
         else:
             print('bad!!!')
 
