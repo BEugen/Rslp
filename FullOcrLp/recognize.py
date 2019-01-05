2ec28603f0ae2c5cebf9bb7a209328f3e632170e
BattsEA <b-eugen@yandex.ru>
2019-01-05 18:58:15 +0300

    LP ocr

--- FullOcrLp/recognize.py
+++ FullOcrLp/recognize.py
@@ -31,7 +31,13 @@ class RecognizeLp(object):
         self.pdl = PREDICT_DETECT_LEVEL
         self.letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
                         'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y', ' ']
+        self.images_arr = []
+        self.index_chars = [12, 27, 40, 52, 62, 75, 90, 102, 112]
 
+    def __images_arr_init(self):
+        self.images_arr = []
+        for i in range(0, LP_MAX_LENGHT):
+            self.images_arr.append([])
 
     def __plot_images(self, images, grey):
         fig = plt.figure(figsize=(15, 18))
@@ -77,7 +83,7 @@ class RecognizeLp(object):
             md_arr.append(md)
             out = img[min_y:max_y + 1, min_x:max_x + 1]
             out = self.__image_rotate(out, i)
-            #out = cv2.resize(out, (128, 64))
+            # out = cv2.resize(out, (128, 64))
             # out = np.expand_dims(out.T, -1)/255
             img_gepotise.append(out)
         mdmax = np.max(md_arr)
@@ -112,63 +118,80 @@ class RecognizeLp(object):
                 cv2.drawContours(img, cntr, idx, (0, 0, 0), -1)
         return img
 
-
-    def __image_denoise(self, image, areapixel=10, radius=3, gamma1=0.2, gamma2=1.5, sigma=10, levelthreh=50):
+    def __image_denoise(self, image, expandpixel=4, areapixel=20, radius=3, gamma1=0.2, gamma2=1.5, sigma=10,
+                        levelthreh=50):
         image = cv2.equalizeHist(image)
         (rows, cols) = image.shape
-        img_expand = np.full((rows + 6, cols), 255)
-        img_expand[3:rows+3, :] = image[:, :]
+        img_expand = np.full((rows + expandpixel*2, cols), 255)
+        img_expand[expandpixel:rows + expandpixel, :] = image[:, :]
         (rows, cols) = img_expand.shape
-        imglogr = np.log1p(np.array(img_expand, dtype='float')/255)
+        imglogr = np.log1p(np.array(img_expand, dtype='float') / 255)
         # Create Gaussian mask of sigma 10
-        M = 2*rows + 1
-        N = 2*cols + 1
-        (X, Y) = np.meshgrid(np.linspace(0, N-1, N), np.linspace(0, M-1, M))
-        centerx = np.ceil(N/2)
-        centery = np.ceil(M/2)
-        gauss_numerator = (X - centerx)**2 + (Y - centery)**2
+        M = 2 * rows + 1
+        N = 2 * cols + 1
+        (X, Y) = np.meshgrid(np.linspace(0, N - 1, N), np.linspace(0, M - 1, M))
+        centerx = np.ceil(N / 2)
+        centery = np.ceil(M / 2)
+        gauss_numerator = (X - centerx) ** 2 + (Y - centery) ** 2
         # Low pass and high pass filter
-        hlow = np.exp(-gauss_numerator/(2*sigma*sigma))
+        hlow = np.exp(-gauss_numerator / (2 * sigma * sigma))
         hhigh = 1 - hlow
         hlowshift = scipy.fftpack.ifftshift(hlow.copy())
         hhighshift = scipy.fftpack.ifftshift(hhigh.copy())
 
         # filter image and crop
         imagefl = scipy.fftpack.fft2(imglogr.copy(), (M, N))
-        imageoutlow = scipy.real(scipy.fftpack.ifft2(imagefl.copy()*hlowshift, (M, N)))
-        imageouthigh = scipy.real(scipy.fftpack.ifft2(imagefl.copy()*hhighshift, (M, N)))
-        imageout = gamma1*imageoutlow[0:rows, 0:cols] + gamma2*imageouthigh[0:rows, 0:cols]
+        imageoutlow = scipy.real(scipy.fftpack.ifft2(imagefl.copy() * hlowshift, (M, N)))
+        imageouthigh = scipy.real(scipy.fftpack.ifft2(imagefl.copy() * hhighshift, (M, N)))
+        imageout = gamma1 * imageoutlow[0:rows, 0:cols] + gamma2 * imageouthigh[0:rows, 0:cols]
 
-        #anti-log rescale to [0, 1]
+        # anti-log rescale to [0, 1]
         imagehmf = np.expm1(imageout)
-        imagehmf = (imagehmf - np.min(imagehmf))/(np.max(imagehmf) - np.min(imagehmf))
-        imagehmf2 = np.array(255*imagehmf, dtype='uint8')
+        imagehmf = (imagehmf - np.min(imagehmf)) / (np.max(imagehmf) - np.min(imagehmf))
+        imagehmf2 = np.array(255 * imagehmf, dtype='uint8')
         imagetreshold = imagehmf2 < levelthreh
         imagetreshold = 255 * imagetreshold.astype('uint8')
         img = self.__image_clear_border(imagetreshold, radius)
         img = self.__bw_area_open(img, areapixel)
         return img
 
-
-    def __get_split_mask(self, image, lp_number, char_size_min=10, char_size=10):
+    def __get_split_mask(self, image, lp_number, char_size_min=10, char_size=10, lfirstindex=30, level_split=12):
         img = cv2.resize(image, (128, 64))
         mean_imgs = np.mean(img, axis=0)
+        fig = plt.figure(figsize=(15, 18))
+        imgplot = plt.imshow(img, cmap='gray')
+        plt.plot(mean_imgs)
         mask_index_split = []
-        i = 0
         fi = int(char_size / 2)
-        index = np.where(mean_imgs[i:i + fi] == np.min(mean_imgs[i:i + fi]))
-        i = np.max(index)
+        index = np.where(mean_imgs >= lfirstindex)
+        i = int(np.min(index) - char_size/2)
+        if i < 0:
+            index = np.where(mean_imgs[i:i + fi] == np.min(mean_imgs[i:i + fi]))
+            i = np.max(index)
         mask_index_split.append(i)
         while (i + char_size) < len(mean_imgs):
             index = np.where(mean_imgs[i:i + char_size] == np.min(mean_imgs[i:i + char_size]))
+            if len(index[0]) == 0:
+                i += char_size
+                continue
             n = np.min(index) + i
+            print(n)
             if len(mask_index_split) and (n - mask_index_split[len(mask_index_split) - 1]) <= char_size_min:
                 i += char_size
             else:
                 mask_index_split.append(n)
                 i = n + int(char_size / 2)
         images = []
+        y = np.full((len(mask_index_split),), 20.0)
+        plt.scatter(mask_index_split, y, c='red', s=40)
+        plt.show()
+        print(mask_index_split)
         for i in range(1, len(mask_index_split)):
+
+            idx = (self.index_chars >= np.uint32(mask_index_split[i-1])) * \
+                 (self.index_chars <= np.uint32(mask_index_split[i]))
+            index = np.max(np.where(idx))
+            print(index, idx)
             images.append(img[:, mask_index_split[i - 1]: mask_index_split[i]])
         return images
 
@@ -183,8 +206,8 @@ class RecognizeLp(object):
                 hw = img.shape[0] * 0.5
         else:
             hwi = hwi[0] if len(hwi) > 0 else []
-            i = int(len(hwi)*0.5)
-            hw = hwi[i: i+1]
+            i = int(len(hwi) * 0.5)
+            hw = hwi[i: i + 1]
         shape_img = img.shape[1] if axis == 0 else img.shape[0]
         index = index[0] if len(index) > 0 else []
         sl = index[index > hw]
@@ -229,7 +252,7 @@ class RecognizeLp(object):
         fb = len(mean_imgs) if fb > len(mean_imgs) else fb
         mean_a = mean_imgs[:fa]
         mean_b = mean_imgs[fb:]
-        print("Ma={0}, Mb={1}".format(len(mean_a), len(mean_b)))
+        # print("Ma={0}, Mb={1}".format(len(mean_a), len(mean_b)))
         index = np.where((mean_a <= np.min(mean_a)) & (mean_a <= level))
         index = 0 if len(index[0]) == 0 else np.max(index)
         l_bot = index if index > 0 else 0
@@ -267,8 +290,6 @@ class RecognizeLp(object):
         plt.imshow(imglp, cmap='gray')
         plt.show()
         imglp = self.__image_denoise(imglp)
-        plt.imshow(imglp, cmap='gray')
-        plt.show()
         images = self.__get_split_mask(imglp, lp_number)
         for i in range(0, len(images)):
             out = self.__img_crop_next_2(images[i], axis=1)
@@ -284,14 +305,15 @@ class RecognizeLp(object):
 
     def __image_ocr(self, images):
         try:
-            images = np.array(images)/255
-            images = np.reshape(images, images.shape + (1, ))
+            images = np.array(images) / 255
+            images = np.reshape(images, images.shape + (1,))
             predict = self.ocrlp.predict_classes(images)
             return predict
         except:
             return None
 
     def recognize(self, image, file):
+        self.__images_arr_init()
         img = self.__detect_lp(image, file)
         if img is not None:
             for im in img:
@@ -345,6 +367,5 @@ class RecognizeLp(object):
         file_name = ''
         for i in range(0, 12):
             random.seed()
-            file_name += LP_LETTERS[random.randint(0, len(LP_LETTERS)-1)]
+            file_name += LP_LETTERS[random.randint(0, len(LP_LETTERS) - 1)]
         return file_name
-
