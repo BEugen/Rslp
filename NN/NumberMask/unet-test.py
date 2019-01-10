bc5f80b5e1ee912b8e9cdcae0c0fe82eebc25f4c
eugen <b-eugen@yandex.ru>
2019-01-11 02:22:47 +0300

    Add split chars

---
+++ NN/NumberMask/unet-test.py
@@ -0,0 +1,162 @@
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+import os
+import cv2
+import glob
+from keras.callbacks import TensorBoard
+from time import time
+import random
+from keras import callbacks
+import argparse
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+IMG_TEST = '/mnt/misk/misk/lplate/lp-un-mask/img'
+PR_RSLT = '/mnt/misk/misk/lplate/lp-un-mask/prd'
+fn = []
+IMG_SIZE = (64, 256)
+
+
+def Unet(size):
+    inputs = Input(size)
+
+    conv1 = BatchNormalization()(inputs)
+    conv1 = Conv2D(32, (3, 3), padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ELU()(conv1)
+    conv1 = Conv2D(32, (3, 3), padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = ELU()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), padding='same')(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = ELU()(conv2)
+    conv2 = Conv2D(64, (3, 3), padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = ELU()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), padding='same')(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = ELU()(conv3)
+    conv3 = Conv2D(128, (3, 3), padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = ELU()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), padding='same')(pool3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = ELU()(conv4)
+    conv4 = Conv2D(256, (3, 3), padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = ELU()(conv4)
+
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), padding='same')(pool4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = ELU()(conv5)
+    conv5 = Conv2D(512, (3, 3), padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = ELU()(conv5)
+
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), padding='same')(up6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = ELU()(conv6)
+    conv6 = Conv2D(256, (3, 3), padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = ELU()(conv6)
+
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), padding='same')(up7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = ELU()(conv7)
+    conv7 = Conv2D(128, (3, 3), padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = ELU()(conv7)
+
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), padding='same')(up8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = ELU()(conv8)
+    conv8 = Conv2D(64, (3, 3), padding='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = ELU()(conv8)
+
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), padding='same')(up9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = ELU()(conv9)
+    conv9 = Conv2D(32, (3, 3), padding='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = ELU()(conv9)
+
+    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def testGenerator(test_path, target_size=IMG_SIZE):
+    file_list = os.listdir(test_path)
+    for i in range(len(file_list)):
+        img = cv2.imread(os.path.join(test_path, os.path.splitext(file_list[i])[0] + '.jpg'), cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            continue
+        img = img / 255
+        img = cv2.resize(img, (target_size[1], target_size[0]))
+        img = np.reshape(img, (1, img.shape[0], img.shape[1], 1))
+        fn.append(file_list[i])
+        yield img
+
+
+def saveResult(save_path, npyfile):
+    for i, item in enumerate(npyfile):
+        img = labelVisualize(0.7, item)
+        cv2.imwrite(os.path.join(save_path, fn[i]), img)
+
+
+def labelVisualize(predict_level, img):
+    img_out = np.zeros(img.shape)
+    img_out[img >= predict_level] = 255
+    return img_out
+
+
+def main(args):
+    model = Unet((IMG_SIZE[0], IMG_SIZE[1], 1))
+    model.load_weights('result-un-nb-nn/weights-08-un-nb-nn.h5')
+
+    testgene = testGenerator(IMG_TEST)
+    results = model.predict_generator(testgene, 3600, verbose=1)
+
+    # save model
+    model_json = model.to_json()
+    with open("model-un-nb-nn.json", "w") as json_file:
+        json_file.write(model_json)
+
+    saveResult(PR_RSLT, results)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--save_dir', default='./result-un-nn')
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--epochs', default=150, type=int)
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    args = parser.parse_args()
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    main(args)
