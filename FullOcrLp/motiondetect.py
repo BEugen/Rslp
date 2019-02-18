bfc43bf245dde2e75f90ad01cc765e77e3c80d71
eugen <b-eugen@yandex.ru>
2019-02-18 22:12:43 +0300

    Add

--- FullOcrLp/motiondetect.py
+++ FullOcrLp/motiondetect.py
@@ -4,7 +4,7 @@ import math
 
 
 class MotionDetect:
-    def __init__(self, blur=15, evlc=7.0, scadr=3):
+    def __init__(self, blur=3, evlc=5.0, scadr=2):
         self.blur = blur
         self.evlc = evlc
         self.older_image = None
@@ -14,13 +14,13 @@ class MotionDetect:
     def evc_detect(self, image):
         image = cv2.resize(image, (64, 64))
         blur = cv2.GaussianBlur(image, (self.blur, self.blur), 0)
-        ret, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
+        ret, thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)
         if self.older_image is None:
             self.older_image = thresh
             return 0.0
-        cv2.imshow('Older', self.older_image)
-        cv2.imshow('New', thresh)
-        evl = np.linalg.norm(thresh - self.older_image)/15
+        diff = cv2.absdiff(thresh, self.older_image)
+        cv2.imshow('New', diff)
+        evl = np.linalg.norm(diff - 0)/1000
         if self.sc_count > self.scadr and evl >= self.evlc:
             self.sc_count = 0
             self.older_image = thresh
