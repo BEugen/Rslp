c53afead85163caf53a9f5c267fc123f764c6713
BattsEA <b-eugen@yandex.ru>
2019-02-19 16:55:46 +0300

    Add send image path

--- FullOcrLp/motiondetect.py
+++ FullOcrLp/motiondetect.py
@@ -4,12 +4,16 @@ import math
 
 
 class MotionDetect:
-    def __init__(self, blur=7, evlc=3.0, scadr=2):
+    def __init__(self, blur=7, evlc=3.0, evll=5.0, fon_count=30, scadr=2):
         self.blur = blur
         self.evlc = evlc
+        self.evll = evll
+        self.fon_cadr_count=fon_count
         self.older_image = None
         self.scadr = scadr
         self.sc_count = 0
+        self.fn_count = fon_count
+
 
     def evc_detect(self, image):
         image = cv2.resize(image, (64, 64))
@@ -21,9 +25,12 @@ class MotionDetect:
         diff = cv2.absdiff(thresh, self.older_image)
         cv2.imshow('New', diff)
         evl = np.linalg.norm(diff - 0)/1000
-        if self.sc_count < self.scadr and evl >= self.evlc:
+        if (self.sc_count < self.scadr and evl >= self.evlc) or self.fn_count <= 0:
             self.sc_count = 0
             self.older_image = thresh
+            self.fn_count = self.evll
+        if evl > self.evll:
+            self.fn_count -= 1
         self.sc_count += 1
         return round(evl, 1)
 
