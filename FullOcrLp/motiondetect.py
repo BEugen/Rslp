0477318b6378e47e3cc5381a4c59b74400d8571b
BattsEA <b-eugen@yandex.ru>
2019-03-12 09:26:25 +0300

    Add denoise motion detector

--- FullOcrLp/motiondetect.py
+++ FullOcrLp/motiondetect.py
@@ -13,15 +13,24 @@ class MotionDetect:
         self.fc_end = fon_count
         self.old_evl = 0.00001
 
+    def bw_area_open(self, image, areapixel=5):
+        img = image.copy()
+        cntr, _ = cv2.findContours(img.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        for idx in np.arange(len(cntr)):
+            area = cv2.contourArea(cntr[idx])
+            if 0 <= area <= areapixel:
+                cv2.drawContours(img, cntr, idx, (0, 0, 0), -1)
+        return img
+
     def evc_detect(self, image):
         image = cv2.resize(image, (64, 64))
         blur = cv2.medianBlur(image, self.blur)
         ret, thresh = cv2.threshold(blur, 45, 255, cv2.THRESH_BINARY)
-
         if self.older_image is None:
             self.older_image = thresh
             return 0.0
         diff = cv2.absdiff(thresh, self.older_image)
+        diff = self.bw_area_open(diff, areapixel=3)
         cv2.imshow('New', diff)
         evl = np.linalg.norm(diff - 0) / 1000.0
         delta = math.fabs(evl - self.old_evl) / evl if evl > 0.0 else 0.0
